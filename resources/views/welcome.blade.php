aca8f7a6af64a552ef0fa5c3cc7895d08f379446
jperez25 <jova9991@gmail.com>
2018-09-18 20:45:26 -0500

    css changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,4 @@
 @extends ('layouts.master')
-<<<<<<< HEAD
-@section('content')
-        <h3 class="text-center text-white pt-5">Login form</h3>
-        <div class="container" >
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="text" name="password" id="password" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-                            </div>
-                            <div id="register-link" class="text-right">
-                                <a href="#" class="text-info">Register here</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-=======
 @section ('content')
 <header
   class="background-cover background-center 1"
@@ -81,7 +53,6 @@
               <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/60RfZDe9To0?rel=0" style="pointer-events: auto;"></iframe>
               </div>
->>>>>>> 1f854603e9e06d31bef7f5146d1b0942a6bc75c6
             </div>
             <!-- <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -89,10 +60,6 @@
             </div> -->
           </div>
         </div>
-<<<<<<< HEAD
-
-@endsection
-=======
       </div>
       </div>
 </header>
@@ -386,4 +353,3 @@
 </div><!-- #page -->
 
 @endsection('content')
->>>>>>> 1f854603e9e06d31bef7f5146d1b0942a6bc75c6
