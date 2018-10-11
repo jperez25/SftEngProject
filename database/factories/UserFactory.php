5b5075e384099d195a8ef37038217ad913763dcf
jperez25 <jova9991@gmail.com>
2018-10-11 16:25:32 -0500

    seeds update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,13 +15,69 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
 
+    $city = $faker->city;
+    $state = $faker->stateAbbr;
+    $coordinates = geocode($city." ".$state);
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'verified' => "1",        
         'password' => bcrypt('password'),
         'remember_token' => str_random(10),
-        'lat' => rand(35,40),
-        'lng' => rand(80,90),
+        'bio' => "test",
+        'city'=> $city,
+        'state'=> $state,
+        'lat' => $coordinates[0],
+        'lng' => $coordinates[1],
     ];
 });
+
+function geocode($address){
+ 
+    // url encode the address
+    $address = urlencode($address);
+     
+    // google map geocode api url
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=".env('GOOGLE_API_KEY');
+ 
+    // get the json response
+    $resp_json = file_get_contents($url);
+     
+    // decode the json
+    $resp = json_decode($resp_json, true);
+ 
+    // response status will be 'OK', if able to geocode given address 
+    if($resp['status']=='OK'){
+ 
+        // get the important data
+        $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+        $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+        $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+         
+        // verify if data is complete
+        if($lati && $longi && $formatted_address){
+         
+            // put the data in the array
+            $data_arr = array();            
+             
+            array_push(
+                $data_arr, 
+                    $lati, 
+                    $longi, 
+                    $formatted_address
+                );
+             
+            return $data_arr;
+             
+        }else{
+            return false;
+        }
+         
+    }
+ 
+    else{
+        echo "ERROR: {$resp['status']}\n";
+        return false;
+    }
+}
